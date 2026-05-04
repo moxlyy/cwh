103619e39919d7b510ad5b299fb6f9903b14dcd3
moxlyy <soapyjoechannel@gmail.com>
2026-05-04 19:47:13 +0000

    fixed html errors

---
+++ script.js
@@ -0,0 +1,8 @@
+// subtle mouse parallax
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 20;
+    const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+    document.querySelector(".links").style.transform =
+        `rotateY(${x}deg) rotateX(${-y}deg)`;
+});
